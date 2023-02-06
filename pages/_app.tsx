c81e3e4ab8839775c2deb71d6e5f31c01841d520
Meet8080 <patelarjun687@gmail.com>
2023-02-05 19:05:03 -0500

    added components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
+import React from 'react';
+import {Layout} from '../components';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+    <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
